a73204138f946d2bf2a9b404f64d423973fea7ae
Eoin O'Brien <eoinobrien910@gmail.com>
2023-11-27 18:54:14 +0000

    chore: install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+/* eslint-env node */
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  ignorePatterns: ["node_modules", "dist"],
+};
